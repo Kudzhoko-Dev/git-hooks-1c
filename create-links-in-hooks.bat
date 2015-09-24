d7b301a7697f436f935c21eba1bdfec54560a84b
Cujoko <cujoko@gmail.com>
2015-09-24 20:39:14 +0900

    Переименовал pre-commit.bat в pre-commit-1c.bat.

--- create-links-in-hooks.bat
+++ create-links-in-hooks.bat
@@ -1,4 +1,4 @@
 @echo off
 mklink ".git/hooks/pre-commit" "%~dp0pre-commit.sample"
-mklink ".git/hooks/pre-commit.bat" "%~dp0pre-commit.bat"
+mklink ".git/hooks/pre-commit-1c.bat" "%~dp0pre-commit-1c.bat"
 git config --local core.quotepath false
