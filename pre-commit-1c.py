c13933203526e4dd461dfdff206b3d8edd0df0fd
Cujoko <cujoko@gmail.com>
2015-09-30 19:51:49 +0900

    Пытаюсь сделать процедуру компилирования из исходников.

--- pre-commit-1c.py
+++ pre-commit-1c.py
@@ -44,6 +44,20 @@ def get_for_processing_files(files: list):
     return result
 
 
+def compile_(input_folder: Path, output_file: Path):
+    temp_source_path = Path(tempfile.mkdtemp())
+    if not temp_source_path.exists():
+        temp_source_path.mkdir(parents=True)
+    else:
+        shutil.rmtree(str(temp_source_path), ignore_errors=True)
+
+    renames_file = input_folder / 'renames.txt'
+
+    with renames_file.open(encoding='cp866') as read:
+        for line in read:
+            print(line)
+
+
 def decompile(exe1c: Path, ib: Path, v8_reader: Path, gcomp: Path, files: list):
     result = []
 
@@ -107,6 +121,9 @@ def main():
     argparser.add_argument('-v', '--version', action='version', version='%(prog)s, ver. {}'.format(__version__))
     argparser.add_argument('--debug', action='store_true', default=False, help='if this option exists then debug mode '
                                                                                'is enabled')
+    argparser.add_argument('--compile', action='store_true', default=False)
+    argparser.add_argument('input_foldername', nargs='?')
+    argparser.add_argument('output_filename', nargs='?')
     args = argparser.parse_args()
 
     if args.debug:
@@ -116,24 +133,30 @@ def main():
         import pydevd
         pydevd.settrace(port=10050)
 
-    added_or_modified_files = get_added_or_modified_files()
-    for_processing_files = get_for_processing_files(added_or_modified_files)
-    if len(for_processing_files) == 0:
-        exit(0)
-    exe_1c = Path(get_setting('General', '1C'))
-    if not exe_1c.exists():
-        raise Exception('Платформа не существует!')
-    ib = Path(get_setting('General', 'IB'))  # fixme
-    if not ib.exists():
-        raise Exception('Сервисной информационной базы не существует!')
-    v8_reader = Path(get_setting('General', 'V8Reader'))
-    if not v8_reader.exists():
-        raise Exception('V8Reader не существует!')
-    gcomp = Path(get_setting('General', 'GComp'))
-    if not gcomp.exists():
-        raise Exception('GComp не существует!')
-    for_indexing_source_folders = decompile(exe_1c, ib, v8_reader, gcomp, for_processing_files)
-    add_to_index(for_indexing_source_folders)
+    if args.compile:
+        input_folder = Path(args.input_foldername)
+        output_file = Path(args.output_filename)
+        compile_(input_folder, output_file)
+
+    else:
+        added_or_modified_files = get_added_or_modified_files()
+        for_processing_files = get_for_processing_files(added_or_modified_files)
+        if len(for_processing_files) == 0:
+            exit(0)
+        exe_1c = Path(get_setting('General', '1C'))
+        if not exe_1c.exists():
+            raise Exception('Платформа не существует!')
+        ib = Path(get_setting('General', 'IB'))  # fixme
+        if not ib.exists():
+            raise Exception('Сервисной информационной базы не существует!')
+        v8_reader = Path(get_setting('General', 'V8Reader'))
+        if not v8_reader.exists():
+            raise Exception('V8Reader не существует!')
+        gcomp = Path(get_setting('General', 'GComp'))
+        if not gcomp.exists():
+            raise Exception('GComp не существует!')
+        for_indexing_source_folders = decompile(exe_1c, ib, v8_reader, gcomp, for_processing_files)
+        add_to_index(for_indexing_source_folders)
 
 
 if __name__ == '__main__':
