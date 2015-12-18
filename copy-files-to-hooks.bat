7a3335ba0ffabc41b5e26ac307a67c3f34b70648
Cujoko <cujoko@gmail.com>
2015-12-19 01:52:55 +0900

    Удалил copy-files-to-hooks.bat и create-links-in-hooks.bat. Они более не нужны.

--- copy-files-to-hooks.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-xcopy "%~dp0pre-commit.sample" ".git/hooks/pre-commit" /F /Y
-xcopy "%~dp0pre-commit-1c.bat" ".git/hooks" /F /Y
-git config --local core.quotepath false
--- create-links-in-hooks.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mklink ".git/hooks/pre-commit" "%~dp0pre-commit.sample"
-mklink ".git/hooks/pre-commit-1c.bat" "%~dp0pre-commit-1c.bat"
-git config --local core.quotepath false
