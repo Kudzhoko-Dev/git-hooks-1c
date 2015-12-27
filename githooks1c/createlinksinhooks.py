3adea7ffdcddc47a3ba60e69b4162277bb2d33b6
Cujoko <cujoko@gmail.com>
2015-12-28 05:30:59 +0900

    Улучшил createlinksinhooks.py. Теперь ссылки сначала удаляются, а потом создаются заново.

--- githooks1c/createlinksinhooks.py
+++ githooks1c/createlinksinhooks.py
@@ -5,20 +5,35 @@ import subprocess
 
 
 def create_links_in_hooks_pre_commit():
-    subprocess.call([
-        'cmd.exe',
-        '/C',
-        'mklink',
-        '.git\\hooks\\pre-commit',
-        str(Path(__file__).parent / 'pre-commit.sample')
-    ])
+    script_dir_path = Path(__file__).parent
+    current_dir_path = Path.cwd() / Path('.git', 'hooks')
+
+    pre_commit_file_path = script_dir_path / 'pre-commit.sample'
+    pre_commit_symbolic_path = current_dir_path / 'pre-commit'
+
+    if pre_commit_symbolic_path.exists() or pre_commit_symbolic_path.is_symlink():
+        pre_commit_symbolic_path.unlink()
 
     subprocess.call([
         'cmd.exe',
         '/C',
         'mklink',
-        '.git\\hooks\\pre-commit-1c.bat',
-        str(Path(__file__).parent / 'pre-commit-1c.bat')
+        str(pre_commit_symbolic_path),
+        str(pre_commit_file_path)
+    ])
+
+    bat_file_path = script_dir_path / 'pre-commit-1c.bat'
+    bat_symbolic_path = current_dir_path / 'pre-commit-1c.bat'
+
+    if bat_symbolic_path.exists() or bat_symbolic_path.is_symlink():
+        bat_symbolic_path.unlink()
+
+    subprocess.call([
+        'cmd.exe',
+        '/C',
+        'mklink',
+        str(bat_symbolic_path),
+        str(bat_file_path)
     ])
 
     subprocess.call([
