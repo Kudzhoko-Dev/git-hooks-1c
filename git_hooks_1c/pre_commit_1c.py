f9174117c0a96e3ec722cf50ebfdfddaf46aed43
Cujoko <cujoko@gmail.com>
2017-04-28 18:28:12 +0900

    Исправил ошибку с импортом Parser из parse_1c_build. Рефакторинг.

--- git_hooks_1c/pre_commit_1c.py
+++ git_hooks_1c/pre_commit_1c.py
@@ -1,7 +1,7 @@
 #! python3
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
-from parse_1c_build import Decompiler
+from parse_1c_build import Parser
 from git_hooks_1c import __version__
 from pathlib import Path
 import re
@@ -13,7 +13,7 @@ import sys
 added_or_modified = re.compile('^\s*(?:A|M)\s+"?(?P<rel_name>[^"]*)"?')
 
 
-def get_added_or_modified_files():
+def get_added_or_modified_file_paths():
     result = []
 
     try:
@@ -25,46 +25,46 @@ def get_added_or_modified_files():
         if line != '':
             match = added_or_modified.match(line)
             if match:
-                added_or_modified_file = Path.cwd() / match.group('rel_name')
-                if added_or_modified_file.name.lower() != 'readme.md':
-                    result.append(added_or_modified_file)
+                added_or_modified_file_path = Path.cwd() / match.group('rel_name')
+                if added_or_modified_file_path.name.lower() != 'readme.md':
+                    result.append(added_or_modified_file_path)
 
     return result
 
 
-def get_for_processing_files(files: list):
+def get_for_processing_file_paths(file_paths: list):
     result = []
 
-    for file in files:
-        if file.suffix.lower() in ['.epf', '.erf', '.ert', '.md']:
-            result.append(file)
+    for file_path in file_paths:
+        if file_path.suffix.lower() in ['.epf', '.erf', '.ert', '.md']:
+            result.append(file_path)
 
     return result
 
 
-def decompile(files: list):
+def parse(file_paths: list):
     result = []
 
-    decompiler = Decompiler()
+    parser = Parser()
 
-    for file in files:
-        source_folder = file.parent / (file.stem + '_' + file.suffix[1:] + '_src')
+    for file_path in file_paths:
+        source_dir_path = file_path.parent / (file_path.stem + '_' + file_path.suffix[1:] + '_src')
 
-        if not source_folder.exists():
-            source_folder.mkdir(parents=True)
+        if not source_dir_path.exists():
+            source_dir_path.mkdir(parents=True)
         else:
-            shutil.rmtree(str(source_folder), ignore_errors=True)
+            shutil.rmtree(str(source_dir_path), ignore_errors=True)
 
-        decompiler.parse(file, source_folder)
+        parser.parse(file_path, source_dir_path)
 
-        result.append(source_folder)
+        result.append(source_dir_path)
 
     return result
 
 
-def add_to_index(files: list):
-    for file in files:
-        exit_code = subprocess.check_call(['git', 'add', '--all', str(file)])
+def add_to_index(dir_paths: list):
+    for dir_path in dir_paths:
+        exit_code = subprocess.check_call(['git', 'add', '--all', str(dir_path)])
         if exit_code != 0:
             exit(exit_code)
 
@@ -72,24 +72,15 @@ def add_to_index(files: list):
 def main():
     argparser = ArgumentParser()
     argparser.add_argument('-v', '--version', action='version', version='%(prog)s, ver. {}'.format(__version__))
-    argparser.add_argument('--debug', action='store_true', default=False, help='if this option exists then debug mode '
-                                                                               'is enabled')
     args = argparser.parse_args()
 
-    if args.debug:
-        import sys
-        sys.path.append('C:\\Python34\\pycharm-debug-py3k.egg')
-
-        import pydevd
-        pydevd.settrace(port=10050)
-
-    added_or_modified_files = get_added_or_modified_files()
-    for_processing_files = get_for_processing_files(added_or_modified_files)
-    if len(for_processing_files) == 0:
+    added_or_modified_file_paths = get_added_or_modified_file_paths()
+    for_processing_file_paths = get_for_processing_file_paths(added_or_modified_file_paths)
+    if len(for_processing_file_paths) == 0:
         exit(0)
 
-    for_indexing_source_folders = decompile(for_processing_files)
-    add_to_index(for_indexing_source_folders)
+    for_indexing_source_dir_paths = parse(for_processing_file_paths)
+    add_to_index(for_indexing_source_dir_paths)
 
 
 if __name__ == '__main__':
