38f409281e2bb328dbd70964f66508a434c6166f
Cujoko <cujoko@gmail.com>
2018-04-07 12:11:47 +0900

    Переработал setup.py. Добавил requirements.txt.

--- setup.py
+++ setup.py
@@ -5,55 +5,33 @@ import git_hooks_1c
 
 setup(
     name='git_hooks_1c',
-
     version=git_hooks_1c.__version__,
-
     description='Git hooks utilities for 1C:Enterprise',
-
-    url='https://github.com/Cujoko/git-hooks-1c',
-
     author='Cujoko',
     author_email='cujoko@gmail.com',
-
-    license='MIT',
-
+    url='https://github.com/Cujoko/git-hooks-1c',
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
-
         'Intended Audience :: Developers',
-
         'License :: OSI Approved :: MIT License',
-
         'Natural Language :: Russian',
-
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-
         'Topic :: Software Development',
         'Topic :: Utilities'
     ],
-
     keywords='1c git pre-commit v8reader v8unpack gcomp',
-
-    install_requires=[
-        'parse-1c-build'
-    ],
-
-    dependency_links=[
-        'http://github.com/Cujoko/parse-1c-build/tarball/master',
-    ],
-
-    packages=find_packages(),
     package_data={
         'git_hooks_1c': [
             'pre-commit.sample',
             'pre-commit-1c.bat'
         ]
     },
-
     entry_points={
         'console_scripts': [
             'gh1c=git_hooks_1c.__main__:run'
         ]
-    }
+    },
+    license = 'MIT'
 )
