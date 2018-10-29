19dad5e19068071ed5aadb4227105508de7414d3
Cujoko <cujoko@gmail.com>
2018-10-30 01:09:06 +0900

    Значительно улучшит setup.py. Теперь при установке пакета он не будет импортироваться для того, чтобы получить версию

--- git_hooks_1c/__init__.py
+++ git_hooks_1c/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '6.0.7'
+# noinspection PyUnresolvedReferences
+from git_hooks_1c.__about__ import __version__
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
+import codecs
+import os
 
 from setuptools import find_packages, setup
 
-import git_hooks_1c
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with codecs.open(os.path.join(here, 'git_hooks_1c', '__about__.py'), 'r', 'utf-8') as f:
+    exec (f.read(), about)
 
 setup(
     name='git_hooks_1c',
-    version=git_hooks_1c.__version__,
+    version=about['__version__'],
     description='Git hooks utilities for 1C:Enterprise',
     author='Cujoko',
     author_email='cujoko@gmail.com',
