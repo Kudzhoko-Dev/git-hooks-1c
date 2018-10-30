6f6f8da07d87ae020aa8f380a21b7f68a36b4475
Cujoko <cujoko@gmail.com>
2018-10-30 21:28:44 +0900

    Улучшит декодирование аргументов командной строки
    Декодирование всех аргументов будет выполняться в одном месте

--- git_hooks_1c/core.py
+++ git_hooks_1c/core.py
@@ -3,10 +3,11 @@ from __future__ import absolute_import, unicode_literals
 
 import sys
 
+from commons.compat import u
 from git_hooks_1c.cli import get_argparser
 
 
 def run():
     argparser = get_argparser()
-    args = argparser.parse_args(sys.argv[1:])
+    args = argparser.parse_args(u(sys.argv[1:], 'cp1251'))
     args.func(args)
