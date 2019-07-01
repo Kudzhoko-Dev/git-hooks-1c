0df288c3bd7e81b4f3188247771c90d140bbdda2
Cujoko <cujoko@gmail.com>
2019-07-02 01:58:11 +0900

    Вернёт назад __about__.py

--- git_hooks_1c/__init__.py
+++ git_hooks_1c/__init__.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 # noinspection PyUnresolvedReferences
-from pathlib import Path
+from loguru import logger
 
 # noinspection PyUnresolvedReferences
-from loguru import logger
-import re
-
-here = Path(__file__).parent.parent
-
-__version__ = '0.0.0'
-with Path(here, 'pyproject.toml').open() as f:
-    version_match = re.search(r'version = "(?P<version>\d+\.\d+\.\d+)"', f.read())
-    if version_match:
-        __version__ = version_match.group('version')
+from git_hooks_1c.__about__ import __version__
